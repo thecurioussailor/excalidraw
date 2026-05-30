b1a44cfa8ed679fdad5584377267100f6c1f17ec
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-05-30 14:56:29 +0530

    zod in common

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import {CreateUserSchema} from "@repo/common/types";
 
 const app = express();
+
+app.post('/signup', (req: Request, res: Response) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+    }
+
+})
+
+app.post('/signin', (req, res) => {
+    
+})
+
+app.post('/room', (req, res) => {
+    
+})
 
 app.listen(3000, () => {
     console.log("Server is listening on port: " + 3000);
